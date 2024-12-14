d9937ed66f8b10168a5e6bb8d86ac7dc2b9ca4ba
Minhu Park <alsgn2003@naver.com>
2024-12-14 15:32:06 +0000

    add keyword argument to pyserini retriever

--- lrage/retrievers/pyserini_retriever.py
+++ lrage/retrievers/pyserini_retriever.py
@@ -2,7 +2,9 @@ from typing import List, Optional
 import json
 
 from pyserini.search.lucene import LuceneSearcher, LuceneImpactSearcher
-from pyserini.search.faiss import FaissSearcher, AutoQueryEncoder
+from pyserini.search.faiss import FaissSearcher
+from pyserini.encode import AutoQueryEncoder
+from pyserini.encode import query_encoder_class_map
 from pyserini.search.hybrid import HybridSearcher
 from pyserini.prebuilt_index_info import TF_INDEX_INFO, IMPACT_INDEX_INFO, FAISS_INDEX_INFO
 
@@ -19,6 +21,14 @@ class PyseriniRetriever(Retriever):
         sparse_index_path: Optional[str] = None,
         faiss_index_path: Optional[str] = None,
         encoder_path: Optional[str] = None,
+        encoder_type: Optional[str] = None,
+        tokenizer_name: Optional[str] = None,
+        device: Optional[str] = None,
+        max_length: Optional[int] = None,
+        pooling: Optional[str] = None, 
+        l2_norm: Optional[str] = None, 
+        prefix: Optional[str] = None, 
+        multimodal=False
     ) -> None:
         self.retriever_type = retriever_type
 
@@ -27,8 +37,45 @@ class PyseriniRetriever(Retriever):
         self.bm25_searcher = LuceneSearcher.from_prebuilt_index(bm25_index_path) if bm25_index_path in TF_INDEX_INFO else LuceneSearcher(bm25_index_path)
 
         self.searcher = self._initialize_searcher(
-            retriever_type, sparse_index_path, faiss_index_path, encoder_path
+            retriever_type, sparse_index_path, faiss_index_path, 
+            encoder_path, encoder_type, tokenizer_name, device, max_length, pooling, l2_norm, prefix, multimodal
         )
+
+    # copy from pyserini.search.faiss.init_query_encoder
+    def _init_query_encoder(self, encoder, encoder_class, tokenizer_name, device, max_length, pooling, l2_norm, prefix, multimodal=False):
+        if encoder:
+            _encoder_class = encoder_class
+
+            # determine encoder_class
+            if encoder_class is not None:
+                encoder_class = query_encoder_class_map[encoder_class]
+            else:
+                # if any class keyword was matched in the given encoder name,
+                # use that encoder class
+                for class_keyword in query_encoder_class_map:
+                    if class_keyword in encoder.lower():
+                        encoder_class = query_encoder_class_map[class_keyword]
+                        break
+
+                # if none of the class keyword was matched,
+                # use the AutoQueryEncoder
+                if encoder_class is None:
+                    _encoder_class = "auto"
+                    encoder_class = AutoQueryEncoder
+
+            # prepare arguments to encoder class
+            kwargs = dict(encoder_dir=encoder, tokenizer_name=tokenizer_name, device=device, prefix=prefix)
+            if _encoder_class == 'sentence' or 'sentence' in encoder:
+                kwargs.update(dict(pooling='mean', l2_norm=True))
+            if _encoder_class == 'contriever' or 'contriever' in encoder:
+                kwargs.update(dict(pooling='mean', l2_norm=False))
+            if _encoder_class == "openai-api" or 'openai' in encoder:
+                kwargs.update(dict(max_length=max_length))
+            if _encoder_class == 'auto':
+                kwargs.update(dict(pooling=pooling, l2_norm=l2_norm, prefix=prefix))
+            if _encoder_class == 'clip' or 'clip' in encoder:
+                kwargs.update(dict(l2_norm=True, prefix=prefix, multimodal=multimodal))
+            return encoder_class(**kwargs)
 
     def _initialize_searcher(
         self,
@@ -36,6 +83,14 @@ class PyseriniRetriever(Retriever):
         sparse_index_path: Optional[str],
         faiss_index_path: Optional[str],
         encoder_path: Optional[str],
+        encoder_type: Optional[str],
+        tokenizer_name: Optional[str],
+        device: Optional[str],
+        max_length: Optional[int],
+        pooling: Optional[str], 
+        l2_norm: Optional[str], 
+        prefix: Optional[str], 
+        multimodal=False
     ):
 
         if retriever_type == 'bm25':
@@ -47,13 +102,15 @@ class PyseriniRetriever(Retriever):
         elif retriever_type == 'dense':
             if faiss_index_path is None or encoder_path is None:
                 raise ValueError("DenseRetriever requires faiss_index_path and encoder_path")
-            return (FaissSearcher.from_prebuilt_index(faiss_index_path, AutoQueryEncoder(encoder_path)) if faiss_index_path in FAISS_INDEX_INFO.keys() 
-                        else FaissSearcher(faiss_index_path, AutoQueryEncoder(encoder_path)))
+            encoder = self._init_query_encoder(encoder_path, encoder_type, tokenizer_name, device, max_length, pooling, l2_norm, prefix, multimodal)
+            return (FaissSearcher.from_prebuilt_index(faiss_index_path, encoder) if faiss_index_path in FAISS_INDEX_INFO.keys() 
+                        else FaissSearcher(faiss_index_path, encoder))
         elif retriever_type == 'hybrid':
             if faiss_index_path is None or encoder_path is None:
                 raise ValueError("HybridRetriever requires faiss_index_path and encoder_path")
-            dense_searcher = (FaissSearcher.from_prebuilt_index(faiss_index_path, AutoQueryEncoder(encoder_path)) if faiss_index_path in FAISS_INDEX_INFO.keys() 
-                                else FaissSearcher(faiss_index_path, AutoQueryEncoder(encoder_path)))
+            encoder = self._init_query_encoder(encoder_path, encoder_type, tokenizer_name, device, max_length, pooling, l2_norm, prefix, multimodal)
+            dense_searcher = (FaissSearcher.from_prebuilt_index(faiss_index_path, encoder) if faiss_index_path in FAISS_INDEX_INFO.keys() 
+                                else FaissSearcher(faiss_index_path, encoder))
             return HybridSearcher(dense_searcher, self.bm25_searcher)
         else:
             raise ValueError(f"Unknown retriever type: {retriever_type}")
