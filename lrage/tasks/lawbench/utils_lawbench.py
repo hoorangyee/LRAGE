17d26e86b7f6afd869cd85fa6a853b6259e1523b
abzb1 <cxv0519@gmail.com>
2025-03-07 10:42:58 +0000

    dedup

--- lrage/tasks/lawbench/utils_lawbench.py
+++ lrage/tasks/lawbench/utils_lawbench.py
@@ -2,7 +2,3 @@ from sklearn.metrics import f1_score
 
 def doc_to_text(doc) -> str:
     return doc["question"]
-
-def lawbench_3_3_f1_score(predictions, references):
-    fscore = f1_score(references, predictions, average="macro")
-    return fscore
